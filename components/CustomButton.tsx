611503740364b5a08c8eed59abe20149ffc0bef0
ryo <>
2023-07-08 23:43:55 +0900

    21:33 まで

---
+++ components/CustomButton.tsx
@@ -0,0 +1,14 @@
+// "use client";
+
+import Image from "next/image";
+
+export const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+    ></button>
+  );
+};

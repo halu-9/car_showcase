6a73c2d0f958ee4f1a59f13521c3d7b61ba9c9f2
ryo <>
2023-07-29 21:04:18 +0900

    Heroイメージが表示されるところまで進行。※28:16

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ export const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, or rent a car - quickly and easily!
+          Find, book, or rent a car -- quickly and easily!
         </h1>
 
         <p className="hero__subtitle">
@@ -24,6 +24,13 @@ export const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+        </div>
+      </div>
+
     </div>
   );
 };

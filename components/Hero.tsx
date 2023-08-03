0d2dc1e99e3a846951be0a03c2d2e691b3a84c45
ryo <>
2023-08-03 21:52:21 +0900

    36:16まで（heroセクションを実装、ナビゲーション実装、フッターモジュール作成）

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,10 +27,10 @@ export const Hero = () => {
 
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
         </div>
       </div>
-
     </div>
   );
 };

e476f1a2cd0d3979ef4eac012def1478bb029d82
ryo <>
2023-09-24 09:17:20 +0900

    次、CarDetailsコンポーネントから

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,7 +3,15 @@
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-export const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+export const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+  isDisabled,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
@@ -11,7 +19,17 @@ export const CustomButton = ({ title, containerStyles, handleClick, btnType }: C
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
